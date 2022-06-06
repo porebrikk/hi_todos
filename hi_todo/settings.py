98e7385da4c6ef7ebb2910f63d691457156567f2
porebrikk <jam.d456@gmail.com>
2022-06-06 20:18:26 +1000

    Change debug setting

--- hi_todo/settings.py
+++ hi_todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y&+^e2z!hzxbm4v7nek6(&=s4!z8w9^$)!=_%1qlx$%rca(z!6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

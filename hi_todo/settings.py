50577e270175d80f01426506b148d02ad1a96a39
porebrikk <jam.d456@gmail.com>
2022-06-07 11:35:15 +1000

    Install psql db

--- hi_todo/settings.py
+++ hi_todo/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'hi_todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER' : 'db_admin',
+        'PASSWORD' : '745691238database',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
